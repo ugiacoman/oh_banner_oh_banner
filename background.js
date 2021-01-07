e390ec3c2d15fb9a7789b611a72f47f23501ce2b
David Solinsky <dmsolinsky@email.wm.edu>
2021-01-06 20:28:24 -0500

    adding more features to automate registration

--- background.js
+++ background.js
@@ -10,5 +10,21 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	        });
   	
   });
-
 });
+
+
+chrome.browserAction.tab.addListener(function (url)){
+    chrome.storage.sync.get(function (items) {
+        if (isBanner(url)) {
+            console.log("LETS GOOOOO");
+        }
+            // chrome.tabs.executeScript({ file: "script.js" }, function () {
+            });
+    });
+});
+
+function isBanner(url) {
+    // regex based on https://stackoverflow.com/a/32730577
+    return url.match(/(https?:\/\/?prod?\.?banner?\.wm?\.edu(\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\(\)\*\+,;\=]*)?)/);
+    // checking https://prod.banner.wm.edu/ssb8/twbkwbis.P_WWWLogin
+}
--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-  chrome.storage.sync.get(function(items) {
-  	// console.log("fuk");
+//if time is add/drop period and am not on the website
+chrome.tabs.update({
+    url: "https://prod.banner.wm.edu/ssb8/twbkwbis.P_WWWLogin"
+});
+chrome.storage.sync.get(function (items) {
+    //usernameBox
+    //passwordBox
+    //submit.Click()
+});
+
+chrome.storage.sync.get(function (items) {
   	//console.log(items.Ccrn1);
   	// var x = document.activeElement.id;
   	// console.log(x);
   	//document.getElementById("crn3").value = 5;
-	   document.getElementById("crn_id1").value = items.Ccrn1;
+	 document.getElementById("crn_id1").value = items.Ccrn1;
      document.getElementById("crn_id2").value = items.Ccrn2;
      document.getElementById("crn_id3").value = items.Ccrn3;
      document.getElementById("crn_id4").value = items.Ccrn4;
@@ -14,5 +23,13 @@
      document.getElementById("crn_id8").value = items.Ccrn8;
      document.getElementById("crn_id9").value = items.Ccrn9;
      document.getElementById("crn_id10").value = items.Ccrn10;
-  	
+
+    var buttons = document.getElementsByTagName('input');
+    for (var i = 0; i < buttons.length; i++) {
+        if (buttons[i].type == 'submit' && buttons[i].value == 'Submit Changes') {
+            //console.log("lol");
+            buttons[i].click();
+            break;
+        }
+    }
   });
